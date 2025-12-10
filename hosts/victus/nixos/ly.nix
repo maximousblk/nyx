3de120a9138cc5c170160860bae561889e793e99
maximousblk <maximousblk@gmail.com>
2025-12-10 21:45:05 +0530

    fix(victus): configure pam service for autologin
    Adds an explicit `ly-autologin` PAM service definition to resolve issues
    where autologin would fail to authenticate.

--- hosts/victus/nixos/ly.nix
+++ hosts/victus/nixos/ly.nix
@@ -1,14 +1,24 @@
 { ... }:
 {
-  services.displayManager.ly.enable = true;
-  services.displayManager.ly.settings = {
-    animation = "colormix";
-    battery_id = "BAT0";
-    bigclock = "en";
-    numlock = true;
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "colormix";
+      battery_id = "BAT0";
+      bigclock = "en";
+      numlock = true;
 
-    auto_login_session = "niri";
-    auto_login_user = "maximousblk";
-    auto_login_service = "ly-autologin";
+      auto_login_user = "maximousblk";
+      auto_login_session = "niri";
+    };
+  };
+
+  security.pam.services.ly-autologin = {
+    text = ''
+      auth       required     pam_permit.so
+      account    include      login
+      password   include      login
+      session    include      login
+    '';
   };
 }
