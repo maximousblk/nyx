70afce22d45acffa9a17fb873a73f8e9084f3361
Ashwin Kumar Yadav <ashwin.y@aftershoot.com>
2025-12-26 18:14:56 +0000

    feat(umbra/parts): add deploy-rs profile for home configuration

--- homes/umbra/parts.nix
+++ homes/umbra/parts.nix
@@ -1,18 +1,31 @@
 {
   self,
+  inputs,
   mkHome,
   withSystem,
   ...
 }:
 {
   flake = withSystem "x86_64-linux" (
-    { ... }:
+    { system, ... }:
     {
+      homeProfiles.umbra = {
+        imports = [ ./home.nix ];
+      };
+
       homeConfigurations.umbra = mkHome {
-        inherit withSystem;
+        inherit system;
         username = "ashwin_y";
         homeDirectory = "/home/ashwin_y/.local/share/distrobox/home/umbra";
-        modules = [ self.homeManagerModules.umbra ];
+        modules = [ self.homeProfiles.umbra ];
+      };
+
+      deploy.nodes.umbra = {
+        hostname = "localhost";
+        profiles.home = {
+          user = "ashwin_y";
+          path = inputs.deploy-rs.lib.${system}.activate.home-manager self.homeConfigurations.umbra;
+        };
       };
     }
   );
