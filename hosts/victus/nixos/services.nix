5a54f6a7cf4562dbe57da6f5fc264302dadfacce
maximousblk <maximousblk@gmail.com>
2026-01-25 22:03:55 +0530

    refactor(victus/services): remove cleanup flatpak repo add service
    - Removes the imperative `flatpak-repo` systemd service.

--- hosts/victus/nixos/services.nix
+++ hosts/victus/nixos/services.nix
@@ -58,13 +58,4 @@
   };
 
   services.flatpak.enable = true;
-  systemd.services.flatpak-repo = {
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
 }
