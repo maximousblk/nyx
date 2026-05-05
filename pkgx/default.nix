1a112cd477f69a23398cc3933a6e44124f64e381
maximousblk <maximousblk@gmail.com>
2026-05-05 10:05:21 +0530

    chore(pkgx): drop unused nur-taskrunner
    - Delete pkgx/nur-taskrunner.nix
    - Remove nur-taskrunner entry from pkgx/default.nix

--- pkgx/default.nix
+++ pkgx/default.nix
@@ -2,9 +2,5 @@
 
 {
   dharmx-walls = pkgs.callPackage ./dharmx-walls.nix { inherit pkgs; };
-  nur-taskrunner = pkgs.callPackage ./nur-taskrunner.nix {
-    inherit pkgs;
-    lib = pkgs.lib;
-  };
   polycat = pkgs.callPackage ./polycat.nix { inherit pkgs; };
 }
