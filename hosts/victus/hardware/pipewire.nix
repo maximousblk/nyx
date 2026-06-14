9dbb5fa08d92e947ad50eea079c03cacd9aff6ee
maximousblk <maximousblk@gmail.com>
2026-06-15 04:28:27 +0530

    refactor(victus/pipewire): rework studio voice filter chain
    - Drop rnnoise-plugin.ladspa from extraLadspaPackages, add lsp-plugins
    - Remove the rnnoise filter node from the studio voice chain
    - Add eq, gate, comp, and limit filter nodes alongside deepfilter
    - Reorder nodes in the file to match signal flow
    - Rewire filter chain links to eq → deepfilter → gate → comp → limit

--- hosts/victus/hardware/pipewire.nix
+++ hosts/victus/hardware/pipewire.nix
@@ -11,8 +11,8 @@
     wireplumber.enable = true;
 
     extraLadspaPackages = [
-      pkgs.rnnoise-plugin.ladspa
       pkgs.deepfilternet
+      pkgs.lsp-plugins
     ];
 
     extraConfig.pipewire = {
@@ -50,13 +50,37 @@
                 nodes = [
                   {
                     type = "ladspa";
-                    name = "rnnoise";
-                    plugin = "librnnoise_ladspa";
-                    label = "noise_suppressor_mono";
+                    name = "eq";
+                    plugin = "lsp-plugins-ladspa";
+                    label = "http://lsp-plug.in/plugins/ladspa/para_equalizer_x16_mono";
                     control = {
-                      "VAD Threshold (%)" = 60.0;
-                      "VAD Grace Period (ms)" = 120;
-                      "Retroactive VAD Grace (ms)" = 90;
+                      "Input gain (G)" = 0.75;
+                      "Output gain (G)" = 1.0;
+                      "Equalizer mode" = 0;
+                      "Filter type 0" = 2;
+                      "Frequency 0 (Hz)" = 110;
+                      "Gain 0 (G)" = 1;
+                      "Filter Width 0 (oct)" = 0.5;
+                      "Filter type 1" = 1;
+                      "Frequency 1 (Hz)" = 180;
+                      "Gain 1 (G)" = 0.9;
+                      "Filter Width 1 (oct)" = 1.0;
+                      "Filter type 2" = 1;
+                      "Frequency 2 (Hz)" = 350;
+                      "Gain 2 (G)" = 0.92;
+                      "Filter Width 2 (oct)" = 1.0;
+                      "Filter type 3" = 1;
+                      "Frequency 3 (Hz)" = 3000;
+                      "Gain 3 (G)" = 1.12;
+                      "Filter Width 3 (oct)" = 1.2;
+                      "Filter type 4" = 3;
+                      "Frequency 4 (Hz)" = 7000;
+                      "Gain 4 (G)" = 0.9;
+                      "Filter Width 4 (oct)" = 1.0;
+                      "Filter type 5" = 4;
+                      "Frequency 5 (Hz)" = 11000;
+                      "Gain 5 (G)" = 1;
+                      "Filter Width 5 (oct)" = 0.6;
                     };
                   }
                   {
@@ -66,21 +90,102 @@
                     label = "deep_filter_mono";
                     control = {
                       "Attenuation Limit (dB)" = 100;
-                      "Min processing threshold (dB)" = -15;
-                      "Max ERB processing threshold (dB)" = 35;
-                      "Max DF processing threshold (dB)" = 35;
+                      "Min processing threshold (dB)" = -8;
+                      "Max ERB processing threshold (dB)" = 30;
+                      "Max DF processing threshold (dB)" = 20;
+                      "Min Processing Buffer (frames)" = 1;
                       "Post Filter Beta" = 0.02;
                     };
                   }
-                ];
-                inputs = [ "rnnoise:Input" ];
-                links = [
                   {
-                    output = "rnnoise:Output";
-                    input = "deepfilter:Audio In";
+                    type = "ladspa";
+                    name = "gate";
+                    plugin = "lsp-plugins-ladspa";
+                    label = "http://lsp-plug.in/plugins/ladspa/gate_mono";
+                    control = {
+                      "Input gain (G)" = 1;
+                      "Output gain (G)" = 1.33;
+                      "Sidechain lookahead (ms)" = 8;
+                      "Sidechain reactivity (ms)" = 60;
+                      "Sidechain preamp (G)" = 1;
+                      "High-pass filter mode" = 0;
+                      "High-pass filter frequency (Hz)" = 10;
+                      "Low-pass filter mode" = 0;
+                      "Low-pass filter frequency (Hz)" = 20000;
+                      "Hysteresis" = 0;
+                      "Curve threshold (G)" = 0.08;
+                      "Curve zone size (G)" = 0.04;
+                      "Hysteresis threshold (G)" = 0.001;
+                      "Hysteresis zone size (G)" = 0.001;
+                      "Attack (ms)" = 12;
+                      "Release (ms)" = 100;
+                      "Hold time (ms)" = 80;
+                      "Reduction (G)" = 0.00025119;
+                      "Makeup gain (G)" = 1;
+                      "Dry gain (G)" = 0;
+                      "Wet gain (G)" = 1;
+                      "Dry/Wet balance (%)" = 100;
+                    };
+                  }
+                  {
+                    type = "ladspa";
+                    name = "comp";
+                    plugin = "lsp-plugins-ladspa";
+                    label = "http://lsp-plug.in/plugins/ladspa/compressor_mono";
+                    control = {
+                      "Input gain (G)" = 1;
+                      "Output gain (G)" = 1.3;
+                      "Sidechain lookahead (ms)" = 2;
+                      "Sidechain reactivity (ms)" = 120;
+                      "High-pass filter mode" = 1;
+                      "High-pass filter frequency (Hz)" = 80;
+                      "Attack threshold (G)" = 0.3;
+                      "Attack time (ms)" = 5;
+                      "Release threshold (G)" = 0.15;
+                      "Release time (ms)" = 80;
+                      "Ratio" = 3;
+                      "Knee (G)" = 0.5;
+                      "Makeup gain (G)" = 1;
+                      "Dry/Wet balance (%)" = 100;
+                    };
+                  }
+                  {
+                    type = "ladspa";
+                    name = "limit";
+                    plugin = "lsp-plugins-ladspa";
+                    label = "http://lsp-plug.in/plugins/ladspa/limiter_mono";
+                    control = {
+                      "Input gain (G)" = 1;
+                      "Output gain (G)" = 1;
+                      "Operating mode" = 0;
+                      "Threshold (G)" = 0.95;
+                      "Knee level (G)" = 1;
+                      "Lookahead (ms)" = 3;
+                      "Attack time (ms)" = 1;
+                      "Release time (ms)" = 15;
+                    };
                   }
                 ];
-                outputs = [ "deepfilter:Audio Out" ];
+                inputs = [ "eq:Input" ];
+                outputs = [ "limit:Output" ];
+                links = [
+                  {
+                    output = "eq:Output";
+                    input = "deepfilter:Audio In";
+                  }
+                  {
+                    output = "deepfilter:Audio Out";
+                    input = "gate:Input";
+                  }
+                  {
+                    output = "gate:Output";
+                    input = "comp:Input";
+                  }
+                  {
+                    output = "comp:Output";
+                    input = "limit:Input";
+                  }
+                ];
               };
             };
           }
